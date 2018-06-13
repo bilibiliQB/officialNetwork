13d98bd27e15c7a4bf6389ccbbddff092d3d6e98
Administrator <Administrator@PC-20180528HGEX>
2018-06-13 18:03:06 +0800

    添加fastjson用来解析json数据

--- src/main/java/com/dute/officialNetwork/OfficialNetworkApplication.java
+++ src/main/java/com/dute/officialNetwork/OfficialNetworkApplication.java
@@ -1,10 +1,42 @@
 package com.dute.officialNetwork;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
 @SpringBootApplication
 public class OfficialNetworkApplication {
+
+	@Bean
+	public HttpMessageConverters fastJsonHttpMessageConverters() {
+		// 1、需先定义一个convert 转换消息对象
+		FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+
+		// 2、添加fastJson 的配置信息，比如：是否要格式化返回的json数据
+		FastJsonConfig fastJsonConfig = new FastJsonConfig();
+		fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+
+		// 2-1、处理中文乱码问题
+		List<MediaType> fastMediaTypes = new ArrayList<>();
+		fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+		fastConverter.setSupportedMediaTypes(fastMediaTypes);
+
+		// 3、在convert中添加配置信息
+		fastConverter.setFastJsonConfig(fastJsonConfig);
+
+		HttpMessageConverter<?> converter = fastConverter;
+		return new HttpMessageConverters(converter);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OfficialNetworkApplication.class, args);
