d5fb0e11019f653803915fbf0d42aed58c101f53
Administrator <Administrator@PC-20180528HGEX>
2018-06-15 10:50:21 +0800

    进行一些测试

--- src/main/java/com/dute/officialNetwork/controller/IndexController.java
+++ src/main/java/com/dute/officialNetwork/controller/IndexController.java
@@ -1,13 +1,46 @@
 package com.dute.officialNetwork.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSONObject;
+import com.dute.officialNetwork.domain.entity.CustomerInformation;
+import com.dute.officialNetwork.service.CustomerInformationService;
+import com.dute.officialNetwork.util.IPUtil;
 
 @Controller
-//测试
+// 测试
 public class IndexController {
+
+	@Autowired
+	private CustomerInformationService cis;
+
 	@RequestMapping("/")
 	public String index() {
 		return "index";
+	}
+
+	@RequestMapping(value = "/save")
+	@ResponseBody
+	public Object save(HttpServletRequest request, @Valid CustomerInformation ci, BindingResult result) {
+		JSONObject json = new JSONObject();
+		// hibernate Validator为快速失败返回模式,返回有且只有一条错误信息
+		if (result.hasErrors()) {
+			json.put("error", result.getAllErrors().get(0).getDefaultMessage());
+		} else {
+			ci.setIP(IPUtil.getIpAddr(request));
+			if (cis.save(ci) != null) {
+				json.put("success", "提交成功");
+			} else {
+				json.put("fail", "提交失败");
+			}
+		}
+		return json;
 	}
 }
