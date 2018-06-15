6459d00bda0dd693227cb660f208ebe02e68481d
Administrator <Administrator@PC-20180528HGEX>
2018-06-15 10:48:53 +0800

    业务接口

---
+++ src/main/java/com/dute/officialNetwork/service/CustomerInformationService.java
@@ -0,0 +1,9 @@
+package com.dute.officialNetwork.service;
+
+import com.dute.officialNetwork.domain.entity.CustomerInformation;
+
+public interface CustomerInformationService {
+	Long save(CustomerInformation customerInformation);
+	
+	
+}
