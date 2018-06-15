d5dc2a4d8440430165ed71b41e7bf264cbe10ae7
Administrator <Administrator@PC-20180528HGEX>
2018-06-15 16:01:29 +0800

    添加参数request

--- src/main/java/com/dute/officialNetwork/service/impl/CustomerInformationServiceImpl.java
+++ src/main/java/com/dute/officialNetwork/service/impl/CustomerInformationServiceImpl.java
@@ -1,11 +1,14 @@
 package com.dute.officialNetwork.service.impl;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dute.officialNetwork.domain.entity.CustomerInformation;
 import com.dute.officialNetwork.domain.repository.CustomerInformationRepository;
 import com.dute.officialNetwork.service.CustomerInformationService;
+import com.dute.officialNetwork.util.IPUtil;
 
 @Service
 public class CustomerInformationServiceImpl implements CustomerInformationService {
@@ -14,9 +17,12 @@ public class CustomerInformationServiceImpl implements CustomerInformationServic
 	private CustomerInformationRepository cir;
 
 	@Override
-	public Long save(CustomerInformation ci) {
-		if (cir.save(ci) != null) {
-			return cir.save(ci).getId();
+	public Long save(CustomerInformation ci, HttpServletRequest request) {
+		if (ci != null) {
+			ci.setIP(IPUtil.getIpAddr(request));
+			if (cir.save(ci) != null) {
+				return cir.save(ci).getId();
+			}
 		}
 		return null;
 	}
