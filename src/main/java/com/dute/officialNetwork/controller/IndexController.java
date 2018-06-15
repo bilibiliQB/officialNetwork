2105852dc5eb3002d829fed74377844b8925cd22
Administrator <Administrator@PC-20180528HGEX>
2018-06-15 16:01:01 +0800

    删除 validation的一些东西

--- src/main/java/com/dute/officialNetwork/controller/IndexController.java
+++ src/main/java/com/dute/officialNetwork/controller/IndexController.java
@@ -1,25 +1,15 @@
 package com.dute.officialNetwork.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
 import com.dute.officialNetwork.api.request.index.TestRequest;
 import com.dute.officialNetwork.api.response.index.TestResponse;
 import com.dute.officialNetwork.util.ResultData;
-import com.google.common.collect.Lists;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.alibaba.fastjson.JSONObject;
-import com.dute.officialNetwork.domain.entity.CustomerInformation;
-import com.dute.officialNetwork.service.CustomerInformationService;
-import com.dute.officialNetwork.util.IPUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,31 +20,10 @@ import java.util.List;
 // 测试
 public class IndexController {
 
-	@Autowired
-	private CustomerInformationService cis;
-
 	@ApiOperation("首页测试")
 	@PostMapping("/")
 	public String index() {
 		return "index";
-	}
-
-	@RequestMapping(value = "/save")
-	@ResponseBody
-	public Object save(HttpServletRequest request, @Valid CustomerInformation ci, BindingResult result) {
-		JSONObject json = new JSONObject();
-		// hibernate Validator为快速失败返回模式,返回有且只有一条错误信息
-		if (result.hasErrors()) {
-			json.put("error", result.getAllErrors().get(0).getDefaultMessage());
-		} else {
-			ci.setIP(IPUtil.getIpAddr(request));
-			if (cis.save(ci) != null) {
-				json.put("success", "提交成功");
-			} else {
-				json.put("fail", "提交失败");
-			}
-		}
-		return json;
 	}
 
 	/**
