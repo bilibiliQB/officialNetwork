a675d390d2644e6fe1c671b4223885195fd5a14e
Administrator <Administrator@PC-20180528HGEX>
2018-06-15 10:51:27 +0800

    修改一些信息

--- src/main/java/com/dute/officialNetwork/domain/entity/CustomerInformation.java
+++ src/main/java/com/dute/officialNetwork/domain/entity/CustomerInformation.java
@@ -2,6 +2,14 @@ package com.dute.officialNetwork.domain.entity;
 
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import com.alibaba.fastjson.annotation.JSONField;
+
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,31 +23,41 @@ public class CustomerInformation {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", updatable = false)
 	private Long id;// ID
-	
-	@Column(nullable = false,name = "ip")
+
+	@Column(name = "ip")
 	private String IP;// IP地址
-	
-	@Column(nullable = true,name = "name")
+
+	@Column(name = "name")
+	@Size(max = 20, message = "姓名不得超过20个字符")
 	private String name;// 客户姓名
-	
-	@Column(nullable = false,name = "phone_number")
+
+	@Column(name = "phone_number")
+	@NotBlank(message = "手机号必须填写")
+	@Pattern(regexp = "^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\\d{8}$", message = "手机号格式错误")
 	private String phoneNumber;// 客户手机号
-	
-	@Column(nullable = true,name = "problem_description")
+
+	@Column(name = "problem_description")
+	@Size(max = 255, message = "问题描述不得超过255字符")
 	private String problemDescription;// 问题描述
-	
-	@Column(nullable = true,name = "house_area")
-	private Integer houseArea;// 房屋面积
-	
-	@Column(nullable = true,name = "houses_name")
+
+	@Column(name = "house_area")
+	@Size(max = 5, message = "房屋面积不得超过5个字符")
+	@Pattern(regexp = "^[0-9]*$", message = "房屋面积只能输入纯数字")
+	private String houseArea;// 房屋面积
+
+	@Column(name = "houses_name")
+	@Size(max = 20, message = "楼盘名称不得超过20个字符")
 	private String housesName;// 楼盘名称
+
+	@JSONField(format = "yyyyMMddHHmmss")
+	@Column(name = "create_time")
+	private Date createTime; // 创建时间
 
 	public CustomerInformation() {
 	}
 
 	public CustomerInformation(Long id, String IP, String name, String phoneNumber, String problemDescription,
-			Integer houseArea, String housesName) {
-		super();
+			String houseArea, String housesName, Date createTime) {
 		this.id = id;
 		this.IP = IP;
 		this.name = name;
@@ -47,6 +65,7 @@ public class CustomerInformation {
 		this.problemDescription = problemDescription;
 		this.houseArea = houseArea;
 		this.housesName = housesName;
+		this.createTime = createTime;
 	}
 
 	public String getIP() {
@@ -89,11 +108,11 @@ public class CustomerInformation {
 		this.problemDescription = problemDescription;
 	}
 
-	public Integer getHouseArea() {
+	public String getHouseArea() {
 		return houseArea;
 	}
 
-	public void setHouseArea(Integer houseArea) {
+	public void setHouseArea(String houseArea) {
 		this.houseArea = houseArea;
 	}
 
@@ -104,4 +123,13 @@ public class CustomerInformation {
 	public void setHousesName(String housesName) {
 		this.housesName = housesName;
 	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
 }
