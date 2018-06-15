58b05b90adf2f56cf62c5537af43786c6ebdb567
Administrator <Administrator@PC-20180528HGEX>
2018-06-15 16:00:17 +0800

    删除validation配置

--- src/main/java/com/dute/officialNetwork/OfficialNetworkApplication.java
+++ src/main/java/com/dute/officialNetwork/OfficialNetworkApplication.java
@@ -3,11 +3,6 @@ package com.dute.officialNetwork;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
-
-import org.hibernate.validator.HibernateValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
@@ -43,17 +38,6 @@ public class OfficialNetworkApplication {
 		HttpMessageConverter<?> converter = fastConverter;
 		return new HttpMessageConverters(converter);
 	}
-	
-	// 配置hibernate Validator为快速失败返回模式
-	@Bean
-    public Validator validator(){
-        ValidatorFactory validatorFactory = Validation.byProvider(HibernateValidator.class )
-                .configure()
-                .addProperty( "hibernate.validator.fail_fast", "true" )
-                .buildValidatorFactory();
-        Validator validator = validatorFactory.getValidator();
-        return validator;
-    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(OfficialNetworkApplication.class, args);
