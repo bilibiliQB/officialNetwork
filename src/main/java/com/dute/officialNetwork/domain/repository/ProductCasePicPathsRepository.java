4f86100ef085089562de367958e8024167e2ae40
bilibiliQB <913424100@qq.com>
2018-06-23 14:27:00 +0800

    根据需求删除无用的东西

--- src/main/java/com/dute/officialNetwork/domain/repository/ProductCasePicPathsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dute.officialNetwork.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dute.officialNetwork.domain.entity.ProductCasePicPaths;
-
-public interface ProductCasePicPathsRepository extends JpaRepository<ProductCasePicPaths, Long> {
-}
--- src/main/java/com/dute/officialNetwork/domain/repository/ProductCaseTypePicPathsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dute.officialNetwork.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dute.officialNetwork.domain.entity.ProductCaseTypePicPaths;
-
-public interface ProductCaseTypePicPathsRepository extends JpaRepository<ProductCaseTypePicPaths, Long> {
-}
