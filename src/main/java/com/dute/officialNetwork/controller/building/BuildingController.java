44814194cd92afc81895d75db1e3b6e4dbccc700
zhw74101 <zhw74101@163.com>
2018-07-19 12:01:07 +0800

    修改热装楼盘完成1

--- src/main/java/com/dute/officialNetwork/controller/building/BuildingController.java
+++ src/main/java/com/dute/officialNetwork/controller/building/BuildingController.java
@@ -7,6 +7,7 @@ import com.dute.officialNetwork.util.ResultData;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class BuildingController {
     @Autowired
     private IBuildingService buildingService;
 
-    @RequestMapping("/getBuildingData")
+    @PostMapping("/getBuildingData")
     @ApiOperation("获取热装楼盘数据")
     public ResultData<BuildingDataResponse> getBuildingData(BuildingPageRequest buildingPageRequest){
         ResultData<BuildingDataResponse> resultData = new ResultData();
